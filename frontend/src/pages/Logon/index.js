3ce9ea884a75883298cd22f195d3b25030cb4b3f
Lucas Ferreira Pinto <Lucas.Ferreira.Pinto1@ibm.com>
2020-04-15 05:16:28 -0300

    Logon page and profiles page were connected with backend

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+import api from '../../services/api'
+
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon(){
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogon (e){
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');        
+        } catch (err) {
+            alert('Erro ao cadastrar nova ong, tente novamente');
+        }
+    }
     return(
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be the Hero"/>
 
-                <form>
+                <form onSubmit={handleLogon}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID" />
+                    <input 
+                        placeholder="Sua ID" 
+                        value={id}
+                        onChange={e => setId(e.target.value)}    
+                    />
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
